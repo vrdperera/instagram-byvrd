d812eb0f2371c110314f9f0eb353c08600fc7521
Vrd Perera <vrdperera@gmail.com>
2022-01-08 20:22:37 +0530

    that was not a mistake, changed again  'Use' to 'use'  in useAuthListener hook

--- src/hooks/useAuthListener.js
+++ src/hooks/useAuthListener.js
@@ -1,7 +1,7 @@
 import { useEffect, useContext, useState } from 'react';
 import FirebaseContext from '../context/firebase';
 
-export default function UseAuthListener() {
+export default function useAuthListener() {
   const [user, setUser] = useState(() =>
     JSON.parse(localStorage.getItem('authUser'))
   );
