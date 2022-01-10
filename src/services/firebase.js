88d521e8db1f121d47a3af19ff5df72f68e75c29
Vrd Perera <vrdperera@gmail.com>
2022-01-10 09:15:25 +0530

    create getUserById function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,24 @@
 import { firebase, FieldValue } from '../lib/firebase';
 
 export async function doesUserNameExsit(username) {
-  console.log(username);
   const results = await firebase
     .firestore()
     .collection('users')
     .where('username', '==', username)
     .get();
 
-  console.log(results);
-
   return results.docs.map((user) => user.data().length > 0);
+}
+
+export async function getUserById(userId) {
+  const results = await firebase
+    .firestore()
+    .collection('users')
+    .where('userId ' === userId)
+    .get();
+
+  const user = results
+    .docs()
+    .map((user) => ({ ...user.data(), docId: user.id }));
+  return user;
 }
