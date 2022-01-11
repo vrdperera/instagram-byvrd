4c8fa2ced54e4f587540f07186e48b2e8c640ee4
Vrd Perera <vrdperera@gmail.com>
2022-01-11 16:53:01 +0530

    add the proptypes to the User component

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,12 @@
-export default function User() {
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+export default function User({ fullName, username }) {
   return <p> Ima User </p>;
 }
+
+User.propTypes = {
+  fullName: PropTypes.string,
+  username: PropTypes.string,
+};
