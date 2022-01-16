28d30e81b29b771e3108f4b46a7c597650329b8a
Vrd Perera <vrdperera@gmail.com>
2022-01-16 19:36:45 +0530

    correct  some bugs

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,18 @@
+import { Skeleton } from '@mui/material';
+import usePhotos from '../hooks/usePhotos';
+
 export default function Timeline() {
+  const { photos } = usePhotos();
+  console.log(photos);
   return (
     <div className="container col-span-2">
-      <p>im the timeline</p>
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, idx) => {
+            return <Skeleton id={idx} />;
+          })}
+        </>
+      ) : null}
     </div>
   );
 }
