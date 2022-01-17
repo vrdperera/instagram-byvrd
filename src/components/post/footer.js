2a9a6bbdccd0c4d98e61fa87822203da78c2feed
Vrd Perera <vrdperera@gmail.com>
2022-01-17 15:29:44 +0530

    Change styles

--- src/components/post/footer.js
+++ src/components/post/footer.js
@@ -4,7 +4,7 @@ export default function Footer({ caption, username }) {
   return (
     <div className="p-4 pt-2 pb-1">
       <span className="mr-1 font-bold">{username}</span>
-      <span className="italic">{caption}</span>
+      <span className="">{caption}</span>
     </div>
   );
 }
