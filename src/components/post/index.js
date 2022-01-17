1f957ec54f13c8128c0ac2cb12f343996363db54
Vrd Perera <vrdperera@gmail.com>
2022-01-17 12:39:40 +0530

    Add image componnet into index post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,13 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Image from './image';
 
-export default function Post({ content: { username } }) {
+export default function Post({ content: { username, imageSrc, caption } }) {
   return (
     <div className="rounded col-span-4  bg-white border border-gray-300 mb-12">
       <Header username={username} />
+      <Image src={imageSrc} caption={caption} />
     </div>
   );
 }
