7c540c394e9b3b975a2f6b237b634765dcfd7159
Vrd Perera <vrdperera@gmail.com>
2022-01-18 19:17:13 +0530

    Create partially profile component

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes.js';
 
 export default function Profile() {
   const { username } = useParams();
-  const [userExist, setUserExist] = useState(undefined);
+  console.log(username);
+  const history = useHistory();
+  const [user, setUser] = useState(null);
+  const [userExist, setUserExist] = useState(false);
 
-  return <h1>HII</h1>;
+  useEffect(() => {
+    async function checkUserExist() {
+      const user = await getUserByUsername(username);
+      if (user.length > 0) {
+        setUser(user);
+        setUserExist(true);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+    checkUserExist();
+    console.log(user);
+  }, [history, username]);
+
+  return username ? <div className="bg-gray-500"></div> : null;
 }
