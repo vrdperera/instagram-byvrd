2572a5eda1071547fba244a42280dbbc8c9b2542
Vrd Perera <vrdperera@gmail.com>
2022-01-21 15:13:03 +0530

    Add more codes

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,32 @@
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import Header from '../components/header';
 import Timeline from '../components/timeline';
 import Sidebar from '../components/sidebar';
+import useUser from '../hooks/useUser';
+import LoggedInUserContext from '../context/loggedInUser';
 
-export default function Dashboard() {
+export default function Dashboard({ user: loggedInUser }) {
+  const { user, setActiveUser } = useUser(loggedInUser.uid);
+
   useEffect(() => {
     document.title = 'Dashboard';
   });
 
   return (
-    <div className="bg-gray-100">
-      <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto  max-w-screen-lg">
-        <Timeline />
-        <Sidebar />
+    <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
+      <div className="bg-gray-100">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto  max-w-screen-lg">
+          <Timeline />
+          <Sidebar />
+        </div>
       </div>
-    </div>
+    </LoggedInUserContext.Provider>
   );
 }
+
+Dashboard.propTypes = {
+  user: PropTypes.object.isRequired,
+};
